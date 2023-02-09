630ee15eaf0db65d2c4d38edb9d19578967f3cc6
amit05c <amitghosh0595c@gmail.com>
2023-02-09 13:03:15 +0530

    first_vue_app

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div>
+        <button v-on:click="inc">+</button>
+        <button v-on:click="dec">-</button>
+        <div>{{count}}</div>
+    </div>
+</template>
+<script>
+export default {
+    name:"Counter-comp",
+    data()
+    {
+        return {
+            count:0
+        }
+    },
+    methods:{
+        inc(){
+            this.count=this.count+1
+        },
+        dec(){
+            this.count=this.count-1
+        }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
